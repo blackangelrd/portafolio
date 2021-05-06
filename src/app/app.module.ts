6a8b626f78409ce74c2965dde555fcda02f74f6e
blackangelrd <the.family186@gmail.com>
2021-05-05 21:33:51 -0400

    agrege componentes portada,body y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Menu1Component } from './componentes/menu1/menu1.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Menu1Component
+    Menu1Component,
+    PortadaComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

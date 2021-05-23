a11a01d6bdf5e89867ba832bc723b34a2e590416
blackangelrd <the.family186@gmail.com>
2021-05-23 19:20:21 -0400

    implementando ruta 001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//importar RouterModule
+import { RouterModule, Routes } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { Menu1Component } from './componentes/menu1/menu1.component';
 import { PortadaComponent } from './componentes/portada/portada.component';
 import { BodyComponent } from './componentes/body/body.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { P001Component } from './proyectos/p001/p001.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+//Path de Rutas
+const appRoutes: Routes = [
+
+{ path: '', component: HomepageComponent},
+{ path: 'p001', component: P001Component},
+{ path: '', redirectTo: '', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +28,15 @@ import { ContactoComponent } from './componentes/contacto/contacto.component';
     PortadaComponent,
     BodyComponent,
     FooterComponent,
-    ContactoComponent
+    ContactoComponent,
+    P001Component,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    //importando el path
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/proyectos/p001/p001.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-p001',
+  templateUrl: './p001.component.html',
+  styleUrls: ['./p001.component.css']
+})
+export class P001Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
